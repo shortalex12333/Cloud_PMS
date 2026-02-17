7fec203bfd24669304fd8dccded6cd2c3128ece8
CelesteOS Yacht System <celesteos@yacht.local>
2026-02-17 16:29:18 -0500

    chore(FE-01-04): add barrel export for media components package
    - Exports MediaRenderer, DocumentCard, and all fileUtils functions
    - Enables clean imports: @/components/media

---
+++ apps/web/src/components/media/index.ts
@@ -0,0 +1,17 @@
+export { MediaRenderer } from './MediaRenderer';
+export type { MediaRendererProps, MediaFile } from './MediaRenderer';
+
+export { DocumentCard } from './DocumentCard';
+export type { DocumentCardProps, DocumentFile } from './DocumentCard';
+
+export {
+  getFileCategory,
+  getFileCategoryFromExtension,
+  getAttachmentKind,
+  formatFileSize,
+  getDocumentIcon,
+  IMAGE_EXTENSIONS,
+  VIDEO_EXTENSIONS,
+  MEDIA_EXTENSIONS,
+  DOCUMENT_EXTENSIONS,
+} from './fileUtils';
