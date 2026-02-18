30af39aa78f4f407f6154273387d069326933b3c
CelesteOS Yacht System <celesteos@yacht.local>
2026-02-18 11:23:51 -0500

    feat(14-04): create handover-export-sections barrel export
    - Exports SignatureCanvas, EditableSectionRenderer, SignatureSection, FinishButton

---
+++ apps/web/src/components/lens/handover-export-sections/index.ts
@@ -0,0 +1,4 @@
+export { SignatureCanvas } from './SignatureCanvas';
+export { EditableSectionRenderer } from './EditableSectionRenderer';
+export { SignatureSection } from './SignatureSection';
+export { FinishButton } from './FinishButton';
